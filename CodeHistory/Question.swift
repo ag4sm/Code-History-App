12e92df3f0ce5e0b5382fb194ac67f6a77387c4f
Josh Scoville <ag4sm@outlook.com>
2023-02-27 23:19:16 -0500

    Refactored code to a new view to reduce repetition

---
+++ CodeHistory/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  CodeHistory
+//
+//  Created by Josh Scoville on 2/26/23.
+//
+
+import Foundation
+
+struct Question {
+    let questionText: String
+    let possibleAnswers: [String]
+    let correctAnswerIndex: Int
+}
